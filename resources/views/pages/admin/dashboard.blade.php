47f4364b3741ad74c72a2b9be3225ffabdb01ab9
Zioo-webo <zioocta52@gmail.com>
2025-11-25 23:21:52 +0700

    Remove all files from repo but keep locally

--- resources/views/pages/admin/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layoutsAdmin.master')
-
-@section('konten')
-<div class="container-fluid dashboard">
-    <h2>Selamat Datang, Admin!</h2>
-    <p>Anda sedang login sebagai admin.</p>
-</div>
-@endsection
